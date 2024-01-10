dcefcf857cff3f2fd3880e687aef9e9047485bd1
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2024-01-10 22:26:02 +0530

    nextDoble()

---
+++ Input/Store_double.java
@@ -0,0 +1,18 @@
+// nextDoble() -> This method stores decimal values with more nunber of significant digits after decimal.
+
+ import java.util.*;
+
+ public class Store_double {
+ 
+	 public static void main(String args[]) {
+		
+		 Scanner sc = new Scanner(System.in);
+
+		 System.out.print("Enter a decimal value : ");
+
+		 double deci = sc.nextDouble(); // double data-type
+
+		 System.out.print("Decimal value : "+ deci);
+
+	}
+ }
