d0c97479bf406279b40f514c5881afa2d50bc096
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2024-01-10 22:13:05 +0530

    nextFloat()

---
+++ Input/Store_decimal.java
@@ -0,0 +1,13 @@
+// nextFloat() -> Take decimal values as input
+
+ import java.util.*;
+
+ public class Store_decimal {
+	
+	 public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the value of pi : ");
+		float pi = sc.nextFloat();
+		System.out.print("Value of pi : "+ pi);
+	}
+ }
