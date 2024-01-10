260d4e483c1124883e17b3665c260b0102993b0c
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2024-01-10 21:01:08 +0530

    Scanner class

---
+++ Input/Class_scanner.java
@@ -0,0 +1,15 @@
+// Input 
+ 
+import java.util.*;
+
+	public class Class_scanner {
+	
+		public static void main(String args[]) {   // main function
+		 Scanner sc = new Scanner(System.in);// Class -> Scanner, Class's object -> sc, new is the keyword and Scanner() is the constructer
+		 System.out.print("Input your name : ");
+		 String name = sc.next();  
+		 System.out.print("Welcome "+ name);
+		
+		}
+	
+	}
