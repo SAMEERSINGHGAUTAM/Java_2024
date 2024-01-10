6214654879aa7baf4b7cfc022f3e0e56f70a7aac
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2024-01-10 21:58:39 +0530

    nextInt()

---
+++ Input/Store_number.java
@@ -0,0 +1,13 @@
+// nextInt() -> To take integer as input
+ import java.util.*;
+
+ public class Store_number {
+	
+	 public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your age : ");
+		int age = sc.nextInt();
+		System.out.print("Your age is : " + age);
+	}
+
+ }
