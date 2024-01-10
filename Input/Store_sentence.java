4b21f595773284008bac38df3542ff68db184bed
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2024-01-10 21:37:50 +0530

    nextLine()

---
+++ Input/Store_sentence.java
@@ -0,0 +1,14 @@
+// nextLine() -> This method stores the string along with spaces as input.
+
+ import java.util.*;
+
+ public class Store_sentence {
+	
+	 public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your full name : ");
+		String name = sc.nextLine();
+		System.out.print("Welcome " + name);
+	}
+
+ }
