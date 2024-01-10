ce92d71e5a47611efd1088b0bdea333ab296398e
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2024-01-10 21:17:29 +0530

    next()

---
+++ Input/Store_string.java
@@ -0,0 +1,21 @@
+// next() - This method stores string as input until it encounters first space
+
+  import java.util.*;
+
+  public class Store_string {
+
+	public static void main(String args[]) {
+
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter your first name");
+		
+		String name = sc.next();
+
+		System.out.print("Welcome " + name);
+
+
+	}
+
+ }
