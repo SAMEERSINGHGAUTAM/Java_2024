92a738c9585aae1e0cadd8c0b821c9393768300c
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2024-01-11 13:37:12 +0530

    nextShort()

---
+++ Input/Store_nextShort.java
@@ -0,0 +1,13 @@
+// nextShort() -> The nextShort() method returns the Short value scanned from the input. Range -> (-32768 to 32767)
+ import java.util.*;
+
+ public class Store_nextShort {
+
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter a value : ");
+		short value = sc.nextShort();
+		System.out.print(value);
+	}
+
+ }
