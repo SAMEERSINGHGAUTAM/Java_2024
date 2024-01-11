38349a7945ce74b1715406243b55f6c61baba29a
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2024-01-11 14:01:24 +0530

    nextLong()

---
+++ Input/Store_nextLong.java
@@ -0,0 +1,15 @@
+// nextLong() - This method Scans the next token of the input as a long. This method returns the long scanned from the input. Range -> -9223372036854775808 to 9223372036854775807
+ import java.util.*;
+
+ public class Store_nextLong {
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter an integer : ");
+
+		long value = sc.nextLong();
+
+		System.out.print(value);
+	}
+ }
