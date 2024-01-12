f8cb38fd48a905b8e023df3c4c77e6b5e42abadd
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2024-01-12 23:31:50 +0530

    Type_casting & size_of_dataType

---
+++ Input/Size_of_dataType.java
@@ -0,0 +1,40 @@
+// Size of all data types
+// DataType.SIZE from java.io.* => bits => bits/8 => bytes
+ import java.util.*;
+ import java.lang.*;
+ import java.io.*;
+//  import static PrimitiveSizes.*;
+
+ public class Size_of_dataType {
+
+	 public static void main(String args[]) {
+		
+		 byte a = 123;
+
+		 short b = 798;
+
+		 int c = 679726;
+
+		 float d = 123.45f;
+
+		 long e = 12563276;
+
+		 double f = 56.678965;
+
+		 String g = "DISCORD";
+
+		 boolean h = true;
+
+ 		System.out.println("Size of int data type : "+ Integer.SIZE/8);
+		System.out.println("Size of byte data type : "+ Byte.SIZE/8);
+		System.out.println("Size of short data type : "+ Short.SIZE/8);
+		System.out.println("Size of long data type : "+ Long.SIZE/8);
+		System.out.println("Size of char data type : "+ Character.SIZE/8);
+		System.out.println("Size of float float type : "+ Float.SIZE/8);
+		System.out.println("Size of double data type : "+ Double.SIZE/8);
+		// System.out.print("Size of int data type : "+ Integer.SIZE/8);
+
+
+	}
+
+}
---
+++ Input/Type_casting.java
@@ -0,0 +1,18 @@
+// Type Casting
+
+ import java.util.*;
+
+ public class Type_casting {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter a value : ");
+		byte a = (byte) sc.nextDouble();
+		System.out.print(a);
+
+		System.out.print("Enter a number : ");
+		char b = (char) sc.next();
+		System.out.print(b);
+	}
+
+ }
