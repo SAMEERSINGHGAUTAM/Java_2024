f73ca8b377d22b17422b6a5373b434f5c3ac6b55
SAMEERSINGHGAUTAM <0808ci211145.ies@ipsacademy.org>
2024-01-16 01:54:28 +0530

    changes

---
+++ Day 1/Relational_Operators/Relational_operator3.java
@@ -0,0 +1,21 @@
+// > (greater than)
+ 
+ import java.util.*;
+ 
+ 	public class Relational_operator3 {
+
+ 		public static void main(String args[]) {
+			
+			Scanner sc = new Scanner(System.in);
+
+			System.out.print("Enter first number : ");
+
+			float val1 = sc.nextFloat();
+
+			System.out.print("Enter the second number : ");
+
+			float val2 = sc.nextFloat();
+
+			System.out.println(val1 > val2);
+ 		}
+	}
